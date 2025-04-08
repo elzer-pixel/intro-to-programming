802ffa2ec7b7b00e0708160dd98655e8d3bbf1c4
elzer-pixel <elzer.mokoena@gmail.com>
2025-04-08 19:40:53 +0200

    commit

---
+++ Book_2_Chapter07/chapter07.js
@@ -0,0 +1,26 @@
+document.getElementById("lefteye").style.backgroundColor = "purple";
+document.getElementById("head").style.transform = "rotate(15deg)";
+
+// put a 2-pixel-wide, solid black border around his body.
+document.getElementById("body").style.border = "2px black solid";
+
+//Round the corners of his mouth.
+document.getElementById("mouth").style.borderRadius = "4px";
+
+//Put yellow dots around his right eye.
+document.getElementById("righteye").style.border = "4px yellow dotted";
+
+// change the text color.
+document.getElementById("body").style.color = "#FF0000";
+// Give Douglas hair.
+document.getElementById("head").style.borderTop = "5px black solid";
+
+// TRY IT OUT:
+
+// Tilting Douglas's head to the other side.
+document.getElementById("head").style.transform = "rotate(-15deg)"
+
+// Make Douglas's nose round.
+document.getElementById("nose").style.borderRadius = "50%";
+//Make Douglas's lips pink.
+document.getElementById("mouth").style.backgroundColor = "pink";
