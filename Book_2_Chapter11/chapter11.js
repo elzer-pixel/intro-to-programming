56a97d561eb7614baad7805ec5c3741a8d4d67b0
elzer-pixel <elzer.mokoena@gmail.com>
2025-04-08 20:40:47 +0200

    commit

---
+++ Book_2_Chapter11/chapter11.js
@@ -0,0 +1,13 @@
+let people = ["Teddy", "Cathy", "Bobby"]
+
+people[0] = "Georgie"
+
+people[0] = "Mary"
+people[1] = "Bobby"
+people[2] = "Judy"
+people[3] = "Eddie"
+people[4] = "Herbie"
+people[5] = "tony"
+
+console.log(people.shift())
+console.log(people)
